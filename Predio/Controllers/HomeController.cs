8854377d2a7a3d8abf699852cbfd348a00c201b9
Alejandro <nichox.ojeda@gmail.com>
2017-12-28 00:14:39 -0300

    first commit

---
+++ Predio/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Predio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Predio.Controllers
+{
+    public class HomeController : Controller
+    {
+        PredioEntities2 cnx;
+
+        public HomeController()
+        {
+            cnx = new PredioEntities2();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Mantenedor()
+        {
+            return View();
+        }
+
+        public ActionResult Guardar(int codigo, string nombre, string sexo, string raza, string promedio)
+        {
+            Vaca listado = new Vaca()
+            {
+                Codigo = codigo,
+                Nombre = nombre,
+                Sexo = sexo,
+                Raza = raza,
+                Promedio = promedio
+            };
+            cnx.Vacas.Add(listado);
+            cnx.SaveChanges();
+
+            return View("Listado", cnx.Vacas.ToList());
+        }
+
+        public ActionResult Listado()
+        {
+
+            return View(cnx.Vacas.ToList());
+        }
+
+        public ActionResult Ficha(int codigo)
+        {
+     
+            return View(cnx.Vacas.Where(x => x.Codigo == codigo).First());
+        }
+
+    }
+}
